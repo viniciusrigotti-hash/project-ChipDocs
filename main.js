f9bc383173581cff71483fee3c9f86aa23b28359
viniciusrigotti-hash <vinicius.rigotti@aluno.senai.br>
2026-08-21 16:44:28 -0300

    0.2

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Script principal
+
+//Declarações
+const header = document.querySelector(".header");
+
+// Header animado
+let ultimaPosicao = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual > ultimaPosicao) {
+        header.style.transform = "translateY(-100%)";
+    }
+    else {
+        header.style.transform = "translateY(0)";
+    }
+
+    ultimaPosicao = posicaoAtual;
+});
